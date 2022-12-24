8c57069a812e57f7dfc29808d0fb7f43151f4ee5
Victorien <victorien.moyret@gmail.com>
2022-12-24 23:13:32 +0100

    fix: 'flightmap'

--- flightmap.py
+++ flightmap.py
@@ -1,49 +1,91 @@
 import csv
 from airport import Airport
 from flight import Flight
+from flightpathbroken import FlightPathBroken
 
 
 class FlightMap:
     def __init__(self):
-        self.airports = []
-        self.flights = []
+        self.airports_list = []
+        self.flights_list = []
         
-    def import_airports(self, csv_file: str)-> None:
-        with open(csv_file, "r") as f:
-            reader = csv.reader(f)
-            next(reader)
-            self.airports = [Airport(name, code, float(lat.replace('"', '')), float(long.replace('"', ''))) 
-                            for name, code, lat, long in reader]
+        
+    def import_airports(self, csv_file: str) -> None:
+        with open(csv_file, 'r') as file:
+            reader = csv.reader(file, quotechar='"', skipinitialspace=True)
+            for row in reader:
+                name, code, lat, longitude = row
+                self.airports_list.append(
+                    Airport(name, code, float(lat), float(longitude)))
+    
     
     def import_flights(self, csv_file: str)-> None:
-        with open(csv_file, 'r') as f:
-            reader = csv.reader(f)
-            next(reader)
-            self.flights = [Flight(src_code, dst_code, float(duration.replace('"', ''))) 
-                           for src_code, dst_code, duration in reader]
+        try:
+            with open(csv_file, 'r') as f:
+                reader = csv.reader(f)
+                next(reader)
+                self.flights_list = [Flight(src_code, dst_code, float(duration.replace('"', ''))) 
+                                        for src_code, dst_code, duration in reader]
+        except:
+            return ('Aucun fichier de vols trouvé')
+            
             
     def airports(self) -> list[Airport]:
-        return self.airports
+        try:
+            return self.airports_list
+        except:
+            return ('Aucune liste d\'aéroports trouvée')
+    
     
     def flights(self) -> list[Flight]:
-        return self.flights
+        try:
+            return self.flights_list
+        except:
+            return ('Aucune liste de vols trouvée')
     
    
     def airport_find(self, airport_code: str) -> Airport:
-        for airport in self.airports:
-            if airport.code == airport_code:
-                return airport
-        return None
-    
+            for airport in self.airports_list:
+                if airport.code == airport_code:
+                    return airport
+            return None
+        
+        
+    def airport_find_error(self, airport_code: str) -> Airport:
+        
+            for airport in self.airports_list:
+                if airport.code == airport_code:
+                    return airport   
+            raise FlightPathBroken("L'aéroport n'a pas été trouvé")
+
+
     def flight_exist(self, src_airport_code: str, dst_airport_code: str) -> bool:
-        flights = [f for f in self.flights if f.src_code == src_airport_code and f.dst_code == dst_airport_code]
-        return bool(flights)
-    
+        for flight in self.flights_list:
+            if flight.src_code == src_airport_code and flight.dst_code == dst_airport_code:
+                return flight
+        return None
+
+
     def flights_where(self, airport_code: str) -> list[Flight]:
-        flights = [f for f in self.flights if f.src_code == airport_code or f.dst_code == airport_code]
-        return flights
+        try:
+            flights = []
+            for flight in self.flights_list:
+                if flight.src_code == airport_code or flight.dst_code == airport_code:
+                    flights.append(flight)
+            return flights
+        except:
+            return ('Aucun trajet trouvé pour ce vol')
+
 
     def airports_from(self, airport_code: str) -> list[Airport]:
-        flights = self.flights_where(airport_code)
-        airports = [self.airport_find(f.dst_code) for f in flights]
-        return airports
+        try:
+            airports = []
+            for flight in self.flights_where(airport_code):
+                if flight.src_code == airport_code:
+                    airport = self.airport_find(flight.dst_code)
+                    if airport:
+                        airports.append(airport)
+            return airports
+        except:
+            return ('Aucun vol en provenance trouvé')
+        
